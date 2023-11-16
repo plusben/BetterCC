10054d98f9ddfc93ff077446a71b8762ad731f2a
Ben Zen <benediktsplintergfx@gmail.com>
2023-11-16 12:20:13 +0100

    Update ebcc.py

--- ebcc.py
+++ ebcc.py
@@ -6,8 +6,7 @@ import time
 cc_launcher_path = "C:\\CCLauncher-Client-3\\CCLauncher_Client_3.0.exe"
 
 def close_cc_launcher():
-    # Ändern Sie diesen Text zum Namen des Fensters des cc-launchers
-    window_title = "CC Launcher 3.0"
+    window_title = "Titel des cc-launcher Fensters"
     windows = gw.getWindowsWithTitle(window_title)
 
     if windows:
@@ -18,17 +17,21 @@ def close_cc_launcher():
         time.sleep(2)  # Wartet, damit das Fenster sicher geschlossen wird
 
 def open_and_login():
-    # Startet den cc-launcher
     subprocess.Popen(cc_launcher_path)
     time.sleep(10)  # Wartet, damit das Programm starten kann
 
-    # Führt den Login-Prozess durch
-    pyautogui.click(x=1000, y=330)  # Klickt auf das Benutzernamen-Feld
-    pyautogui.write('XXXXXXXX')  # Gibt den Benutzernamen ein
-    pyautogui.click(x=1000, y=390)  # Klickt auf das Passwort-Feld
-    pyautogui.write('XXXXXXXX')  # Gibt das Passwort ein
-    pyautogui.click(x=1150, y=450)  # Klickt auf den Login-Button
+    pyautogui.click(x=1000, y=330)
+    pyautogui.write('XXXXX')
+    pyautogui.click(x=1000, y=390)
+    pyautogui.write('XXXXX')
+    pyautogui.click(x=1150, y=450)
 
-# Schließt den cc-launcher, falls er geöffnet ist, und startet ihn neu
-close_cc_launcher()
-open_and_login()
+def run_continuously(interval):
+    while True:
+        close_cc_launcher()
+        open_and_login()
+        time.sleep(interval)  # Wartet eine bestimmte Zeit bis zum nächsten Zyklus
+
+# Setzt das Intervall in Sekunden, z.B. 7200 Sekunden für 2 Stunden
+interval = 7200
+run_continuously(interval)
