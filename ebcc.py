49341d559953d5b9cb5242aa3e81b8acb5329e76
Ben Zen <benediktsplintergfx@gmail.com>
2023-11-16 13:57:29 +0100

    Update ebcc.py
    Eingabe jetzt nur noch wenn das Fenster Login den Fokus hat.

--- ebcc.py
+++ ebcc.py
@@ -4,9 +4,10 @@ import pygetwindow as gw
 import time
 
 cc_launcher_path = "C:\\CCLauncher-Client-3\\CCLauncher_Client_3.0.exe"
+login_window_title = "Login"  # Ändern Sie dies zum exakten Titel des Login-Fensters
 
 def close_cc_launcher():
-    window_title = "Titel des cc-launcher Fensters"
+    window_title = "CC Launcher 3.0"
     windows = gw.getWindowsWithTitle(window_title)
 
     if windows:
@@ -20,11 +21,20 @@ def open_and_login():
     subprocess.Popen(cc_launcher_path)
     time.sleep(10)  # Wartet, damit das Programm starten kann
 
-    pyautogui.click(x=1000, y=330)
-    pyautogui.write('XXXXX')
-    pyautogui.click(x=1000, y=390)
-    pyautogui.write('XXXXX')
-    pyautogui.click(x=1150, y=450)
+    # Findet und aktiviert das Login-Fenster
+    login_window = gw.getWindowsWithTitle(login_window_title)
+    if login_window:
+        login_window[0].activate()
+        time.sleep(2)  # Wartet, bis das Fenster aktiv ist
+
+        # Hier erfolgen die Eingaben
+        pyautogui.click(x=1000, y=330)
+        pyautogui.write('Benutzername')  # Ersetzen Sie dies mit dem tatsächlichen Benutzernamen
+        pyautogui.click(x=1000, y=390)
+        pyautogui.write('Passwort')  # Ersetzen Sie dies mit dem tatsächlichen Passwort
+        pyautogui.click(x=1150, y=450)
+    else:
+        print("Login-Fenster wurde nicht gefunden.")
 
 def run_continuously(interval):
     while True:
@@ -32,6 +42,5 @@ def run_continuously(interval):
         open_and_login()
         time.sleep(interval)  # Wartet eine bestimmte Zeit bis zum nächsten Zyklus
 
-# Setzt das Intervall in Sekunden, z.B. 7200 Sekunden für 2 Stunden
-interval = 7200
+interval = 7200  # 2 Stunden
 run_continuously(interval)
